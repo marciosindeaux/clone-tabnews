a28bece747c2cdc43d54c91ab5100aa372cbe249
Marcio Alexandre Freire Sindeaux <sindeaux1999@hotmail.com>
2025-11-30 18:24:57 -0300

    Editing Index Component

---
+++ pages/produtos/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Frase de Impacto do Ammend - Teste para ver se um Amend pode apagar outras branches que estão apontando para eses ponto na história</h1>
+}
+
+export default Home;
