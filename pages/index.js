a40f455b3bcd4905bb09586343d2470df1631e37
Marcio Alexandre Freire Sindeaux <sindeaux1999@hotmail.com>
2025-12-08 19:52:03 -0300

    ajustando frases

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Main() {
+    return <h1>confira a url com /produtos para verficar a mensagem</h1>
+}
+
+export default Main;
--- pages/produtos/index.js
+++ pages/produtos/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Frase de Impacto do Ammend - Teste para ver se um Amend pode apagar outras branches que estão apontando para eses ponto na história</h1>
+    return <h1>Ajustando a frase</h1>
 }
 
 export default Home;
