38ba7488915653ccc1b72f1684831ed15f332e1d
Marcio Alexandre Freire Sindeaux <sindeaux1999@hotmail.com>
2025-12-09 17:05:04 -0300

    ajustando confiugração do prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Main() {
-    return <h1>confira a url com /produtos para verficar a mensagem</h1>
+    return <h1>confira a url com /produtos para verficar a mensagem</h1>;
 }
 
 export default Main;
