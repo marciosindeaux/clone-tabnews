897bc4c99dbc915c014e68f25e991d334fc3e45d
Marcio Alexandre Freire Sindeaux <sindeaux1999@hotmail.com>
2026-01-01 18:02:34 -0300

    fix: ajustando erro quando a conexão não é estabelecida e incrementado variavel de SSL

--- src/backend/resources/database/postgresql/config/database.js
+++ src/backend/resources/database/postgresql/config/database.js
@@ -1,37 +1,39 @@
 import { Client } from "pg";
+
 export default {
     query: executeQuery,
 };
 
 async function executeQuery(query) {
     if (process.env.DATABASE_ENABLED === "true") {
+        const client = await generateConnection();
+        try {
+            const response = await client.query(query);
+            await client.end();
+            return response;
+        } catch (error) {
+            console.log("Database query error:", error);
+            throw error;
+        } finally {
+            await client.end();
+        }
+    }
+}
+
+async function generateConnection() {
+    try {
         const client = new Client({
             host: process.env.POSTGRES_HOST,
             port: process.env.POSTGRES_PORT,
             user: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
+            ssl: process.env.POSTGRES_SSL === "true",
         });
-
-        try {
-            // return client.query(query).then(
-            //     (queryResult) => {
-            //         client.end();
-            //         return queryResult;
-            //     },
-            //     (error) => {
-            //         client.end();
-            //         throw error;
-            //     },
-            // );
-            await client.connect();
-            const response = await client.query(query);
-            await client.end();
-            return response;
-        } catch (error) {
-            console.log("Database query error:", error);
-        } finally {
-            await client.end();
-        }
+        await client.connect();
+        return client;
+    } catch (error) {
+        console.log("Database connection error:", error);
+        throw error;
     }
 }
